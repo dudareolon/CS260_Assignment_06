6d897a47cc3a74bddb3682761ce5c4ef33bb5ccd
Eduarda (Duda) Reolon <duda.reolon.1589@mail.linnbenton.edu>
2024-02-25 12:16:40 -0800

    initializing tree and root

---
+++ binary_search_tree.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "binary_search_tree.h"
+
+using std::cout;
+using std::endl;
+
+// Initializes the binary search tree under the premises of the class defined in the binary_search_tree.h file
+BinarySearchTree::BinarySearchTree() {
+    root = nullptr; // initializes the tree empty by setting the root to nullptr
+}
+
