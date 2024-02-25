67e43fe23b868c0cbc20dd6d89291c68d2286670
Eduarda (Duda) Reolon <duda.reolon.1589@mail.linnbenton.edu>
2024-02-25 12:05:23 -0800

    binary_node struct

---
+++ binary_node.h
@@ -0,0 +1,9 @@
+
+struct BinaryNode
+// this struct defines what the binary node is
+{
+    // each binary node contains an integer value, a pointer to the address of the node to the left of it and another one to the address to the right of it
+    int value; 
+    BinaryNode *left;
+    BinaryNode *right;
+};
