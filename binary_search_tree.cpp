529b444b032cc6b2499a62441eeb4f6deb7ee5e2
Eduarda (Duda) Reolon <duda.reolon.1589@mail.linnbenton.edu>
2024-02-26 20:20:44 -0800

    main.cpp file done

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -38,6 +38,96 @@ void BinarySearchTree::add(int new_value) {
     }
 }
 
+bool BinarySearchTree::search(int value){
+    // result with a safe default value
+    bool result = false;
+    // start current at root
+    BinaryNode *current = root;
+    //check for nullptr
+    while(current != nullptr) {
+        // check current->value
+        if(value == current->value) {
+            result = true;
+            break;
+        } else if(value < current->value) {
+        // move based on being less than or greater than root->value
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+
+    }
+
+    return result;
+}
+
+int BinarySearchTree::remove(int old_value) {
+    BinaryNode* parent = nullptr;
+    BinaryNode* current = root;
+
+    // Search for the node to be removed and keep track of its parent
+    while (current != nullptr && current->value != old_value) {
+        parent = current;
+        if (old_value < current->value) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+
+    // If the value is not found, return -1
+    if (current == nullptr) {
+        cout << "the number entered is not in the tree" << endl;
+        return -1;
+    }
+
+    // Case 1: Node to be removed has no children or one child
+    if (current->left == nullptr || current->right == nullptr) {
+        BinaryNode* temp = nullptr;
+        if (current->left != nullptr){
+            temp = current->left;
+        } else if (current -> right != nullptr) {
+            temp = current->right;
+        } else {
+            if (parent->left == current){
+                parent->left = nullptr;
+            } else if (parent->right == current){
+                parent->right = nullptr;
+            }
+        }      
+
+        if (parent->left == current) {
+            parent->left = temp;
+        } else {
+            parent->right = temp;
+        }
+        delete current;
+    }
+    // Case 3: Node to be removed has two children
+    else {
+        BinaryNode* successorParent = current;
+        BinaryNode* successor = current->right;
+        while (successor->left != nullptr) {
+            successorParent = successor;
+            successor = successor->left;
+        }
+        current->value = successor->value;
+
+       if (successorParent == current) {
+            current->right = successor->right;
+        } else {
+            // this takes into consideration that even though the successor points to nullptr on its left, there might still be a node on its right
+            // if there is a node on its right then that node will become the left side of the successor parent, 
+            // since it is the only thing left on the left branch of the successor parent
+            successorParent->left = successor->right;
+        }
+
+        delete successor;
+    }
+    return 0;
+}
+
+
 void BinarySearchTree::in_order_traversal() {
     in_order_traversal_helper(root);
 }
@@ -47,7 +137,7 @@ void BinarySearchTree::in_order_traversal_helper(BinaryNode *current) {
         // go left
         in_order_traversal_helper(current->left);
         // visit
-        cout << current->value << endl;
+        cout << current->value << " ";
         // go right
         in_order_traversal_helper(current->right);
     }
--- main.cpp
+++ main.cpp
@@ -6,32 +6,72 @@ using std::cout;
 using std::endl;
 
 int main() {
-    cout << "create bst" << endl;
+
     BinarySearchTree bst;
 
-        
-    // add and test add
-    cout << "add 42" << endl;
-    bst.add(42);
+    // Remove a number when there are no numbers in the tree
+    bst.remove(20);
+    cout<<endl;
 
-    //test adding left
-    cout << "add 20" << endl;
+    // Add numbers to the tree
     bst.add(20);
-    //cout << "test add left: bst.search(20): " << bst.search(20) << endl;
+    bst.add(40);
+    bst.add(10);
+    bst.add(8);
+    bst.add(9);
+    bst.add(15);
+    bst.add(9);
+    bst.add(30);
+    bst.add(25);
+    bst.add(27);
+    bst.add(28);
+    bst.add(1);
 
-    //test add right
-    cout << "add 65" << endl;
-    bst.add(65);
-    //cout << "test add right: bst.search(65): " << bst.search(65) << endl;
-
-    //test add duplicate
-    cout << "add duplicate 20" << endl;
+    // Add a duplicate number
     bst.add(20);
-    //cout << "test add duplicate: bst.search(20): " << bst.search(20) << endl;
+    bst.add(40);
 
-    //test traversal
+
+
+    // Display the tree numbers in order traversal (Ascending)
     cout << "test traversal:" << endl;
     bst.in_order_traversal();
+    cout << endl;
+    cout << endl;
 
+    // Search for a number in the tree
+    cout << "search for a number that is in the tree (20) will return a 1:  "  << bst.search(20) << endl;
+    cout << endl;
+
+    // Remove a number in the tree
+    bst.remove(1);
+    cout << "tree after removing number 1: ";
+    bst.in_order_traversal(); 
+    cout << endl << endl; 
+
+    // Try to search for the number that was removed from the tree
+    cout << "search for a number that is not in the tree (1) will return a 0:  "  << bst.search(1) << endl << endl;
+
+    // Remove a number that it is not in the tree
+    // Should not change the tree
+    bst.in_order_traversal();
+    cout << endl;
+    bst.remove(3);
+    cout << " so tree has not change as you can see below: " << endl;
+    bst.in_order_traversal();
+    cout << endl << endl;
+
+    // Remove a number that the successor has a node to the right of it
+    bst.remove(10);
+    cout << "tree after removing 10: ";
+    bst.in_order_traversal();
+    cout << endl << endl;
+
+    // Remove a number whose successor is to the right of it 
+    bst.remove(27);
+    cout << "tree after removing 27: ";
+    bst.in_order_traversal();
+    cout << endl << endl;
+        
     return 0;
 }
